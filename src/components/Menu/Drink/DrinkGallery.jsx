c0ede66e53163cd98140142020a7aa241c101947
thanusiyanthan28 <thanusiyanthan@iykons.com>
2024-09-28 00:02:15 +0530

    update

--- src/components/Menu/Drink/DrinkGallery.jsx
+++ src/components/Menu/Drink/DrinkGallery.jsx
@@ -31,8 +31,8 @@ const DrinkGallery = () => {
 
             {/* Conditionally Render Components */}
             {activeButton === 'juice' && <div><Juice /></div>}
-            {activeButton === 'fromTheFridge' && <FromTheFridge />}
-            {activeButton === 'water' && <Water />}
+            {activeButton === 'fromTheFridge' && <Juice />}
+            {activeButton === 'water' && <Juice />}
         </div>
     );
 };
